12b1582c16e56942fc6735569483a96e8a842184
Amichenqq <cqq2392133873@gamil.com>
2020-11-06 11:32:43 +0800

    smart compus init

---
+++ src/main/java/com/newer/sc/SmartCompusApplication.java
@@ -0,0 +1,13 @@
+package com.newer.sc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCompusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCompusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/newer/sc/SmartCompusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newer.sc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCompusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
