e45e860a33febcd5dd49710f54abc821c60ee0cf
不掉头发的程序猿 <71914638+lzj1289140507@users.noreply.github.com>
2020-11-13 18:51:53 +0800

    教务的一部分功能 测试通过

--- src/main/java/com/newer/sc/common/mapper/test.java
+++
@@ -1,5 +0,0 @@
-package com.newer.sc.common.mapper;
-
-public class test {
-
-}
---
+++ src/main/java/com/newer/sc/manager/service/ManagerService.java
@@ -0,0 +1,9 @@
+package com.newer.sc.manager.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ManagerService {
+  
+
+}
